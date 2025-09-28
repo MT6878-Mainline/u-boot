f257ea397dec9afe1b8c04f226c90596319ed6c7
Igor Belwon <igor.belwon@mentallysanemainliners.org>
2025-09-28 20:57:04 +0200

    mediatek: mt6878: mt6878_tetris: enable poweroff from fastboot
    Signed-off-by: Igor Belwon <igor.belwon@mentallysanemainliners.org>

--- board/mediatek/mt6878/mt6878_tetris.c
+++ board/mediatek/mt6878/mt6878_tetris.c
@@ -7,5 +7,21 @@
 #include <env.h>
 #include <init.h>
 #include <asm/global_data.h>
+#include <asm/system.h>
+#include <fastboot.h>
+#include <string.h>
 
 DECLARE_GLOBAL_DATA_PTR;
+
+void __weak fastboot_oem_board(char *cmd_parameter, void *data, u32 size, char *response)
+{
+	if(strncmp(cmd_parameter, "poweroff", 10) == 0)
+	{
+		fastboot_okay("goodbye :(", response);
+		psci_system_off();
+	}
+	else
+	{
+		fastboot_fail("unknown oem_board command", response);
+	}
+}
