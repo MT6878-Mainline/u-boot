041483de53a2f8027d16b5ca0e737d2f44ac443e
Igor Belwon <igor.belwon@mentallysanemainliners.org>
2025-09-28 11:20:12 +0200

    board: mediatek: Add CMF Phone 1 (tetris) support
    The CMF Phone 1 is a midrange Android phone released by Nothing
    Technology in 2024. Add support for it, when booting u-boot as lk.
    
    NOTE: CONFIG_OF_EMBED is enabled because of the specific boot process to
    u-boot that this device uses. We cannot pass a dtb to u-boot due to
    limitations of how this device loads and launches u-boot (simply jumps
    to the address from TEE, which cannot be modified)
    
    Signed-off-by: Igor Belwon <igor.belwon@mentallysanemainliners.org>

---
+++ board/mediatek/mt6878/mt6878_tetris.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2025 Igor Belwon <igor.belwon@mentallysanemainliners.org>
+ */
+
+#include <config.h>
+#include <env.h>
+#include <init.h>
+#include <asm/global_data.h>
+
+DECLARE_GLOBAL_DATA_PTR;
---
+++ include/configs/mt6878.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration for MT6878 boards
+ *
+ * Copyright (C) 2024 Igor Belwon
+ */
+
+#ifndef __MT6878_H
+#define __MT6878_H
+
+#endif // __MT6878_H
